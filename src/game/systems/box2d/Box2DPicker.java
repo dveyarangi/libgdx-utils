15fc4b561230401648ae78a94b6a5b439b6ed084
dveyarangi <fima.rotter@gmail.com>
2020-03-28 00:04:30 +0300

    allow setting entity filter when picking

--- src/game/systems/box2d/Box2DPicker.java
+++ src/game/systems/box2d/Box2DPicker.java
@@ -1,12 +1,13 @@
 package game.systems.box2d;
 
-import game.systems.EntityDef;
-import game.world.IPickProvider;
-
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.QueryCallback;
 import com.badlogic.gdx.physics.box2d.World;
+
+import game.systems.EntityDef;
+import game.world.IPickProvider;
+import lombok.Setter;
 
 /**
  * Picks a unit using box2d query
@@ -18,11 +19,17 @@ public class Box2DPicker implements IPickProvider
 	private World world;
 
 	private Entity pickedEnitity;
+	
+	/**
+	 * can be set to filter results of picking query
+	 */
+	@Setter private IEntityFilter entityFilter;
 
 	public Box2DPicker( World world )
 	{
 		this.world = world;
 	}
+	
 
 	/** This is callback for mouse picking test */
 	QueryCallback pickingCallback = new QueryCallback()
@@ -37,7 +44,7 @@ public class Box2DPicker implements IPickProvider
 			if(testedEntity == null)
 				return true;
 			EntityDef def = EntityDef.get( testedEntity );
-			if(def.isPickable())
+			if(def.isPickable() && (entityFilter == null || entityFilter.accept(testedEntity)))
 			{
 				pickedEnitity = testedEntity;
 				return false;
--- src/game/world/IPickProvider.java
+++ src/game/world/IPickProvider.java
@@ -2,9 +2,27 @@ package game.world;
 
 import com.badlogic.ashley.core.Entity;
 
+import game.systems.box2d.IEntityFilter;
+
+/**
+ * Interface for classes that provide entity picking by mouse
+ *
+ */
 public interface IPickProvider
 {
-
+	/**
+	 * Allows setting a filter for entities picked
+	 * @param filter
+	 */
+	public void setEntityFilter(IEntityFilter filter);
+	
+	/**
+	 * Do pick
+	 * @param x
+	 * @param y
+	 * @param pickRadius
+	 * @return entity matching filter (if set) in the specified radius; or null if none found
+	 */
 	Entity pick( float x, float y, float pickRadius );
 
 }
