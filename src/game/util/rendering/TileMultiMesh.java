a328190a0e833737df79f8601f3abc7fd783fd1c
dveyarangi <fima.rotter@gmail.com>
2020-08-31 18:41:29 +0300

    Update TileMultiMesh.java

--- src/game/util/rendering/TileMultiMesh.java
+++ src/game/util/rendering/TileMultiMesh.java
@@ -32,6 +32,7 @@ public class TileMultiMesh
 	private int componentsPerTile;
 
 	@Getter List <Mesh> meshes = new ArrayList <> ();
+	private float [] vertexBufferUpdate;
 
 	/**
 	 * 
@@ -59,6 +60,7 @@ public class TileMultiMesh
 		// defining max tiles for mesh:
 		this.maxTilesPerMesh = 256*256/verticesPerTile;
 		
+		this.vertexBufferUpdate = new float [componentsPerTile];
 
 	}
 	
@@ -66,7 +68,7 @@ public class TileMultiMesh
 	public void fillTiles(TileFill fill)
 	{
 		int remainingTiles = w * h;
-		int tilesPerMesh = Math.min(maxTilesPerMesh, w*h) / 2;
+		int tilesPerMesh = Math.min(maxTilesPerMesh, w*h);
 
 		/////////////////////////////
 		// preallocating buffers for first mesh
@@ -77,29 +79,30 @@ public class TileMultiMesh
 		int indexBufferSize = tilesPerMesh * indicesPerTile;
 		short [] indexBuffer = new short [ indexBufferSize ];
 		
-		int tileIdx = 0; // tile index
-		int iidx = 0; // current index buffer pointer
-		int vidx = 0; // current  vertex buffer pointer
-		
 		/////////////////////////////
 		// iterating over tiles
 		for(int y = 0; y < h; y ++)
 			for(int x = 0; x < w; x ++)
 			{
+				int globalTileIdx = y * w + x;
+				// tile index
+				int tileIdx = globalTileIdx % maxTilesPerMesh;
+				// current index buffer pointer
+				int vidx = tileIdx * componentsPerTile;
+				// current  vertex buffer pointer
+				int iidx = tileIdx * indicesPerTile;
+				
 				/////////////////////////////
 				// fill current tile
 				FloatArrayWindow vertexBufferView = new FloatArrayWindow(vertexBuffer, vidx, componentsPerTile);
 				ShortArrayWindow indexBufferView = new ShortArrayWindow(indexBuffer, iidx, indicesPerTile);
 				
-				fill.fillTile(x, y, tileIdx, vertexBufferView, indexBufferView);
+				fill.fillTile(x, y, tileIdx, vertexBufferView, indexBufferView);	
 				
-				tileIdx ++;
-				vidx += componentsPerTile;
-				iidx += indicesPerTile;
 				
 				/////////////////////////////
 				// instantiating and swapping mesh if full or last
-				if(vidx >= vertexBufferSize || (y == h-1 && x == w-1))
+				if((tileIdx+1) * componentsPerTile >= vertexBufferSize || (y == h-1 && x == w-1))
 				{
 					// create mesh and store it
 					Mesh mesh = new Mesh( true, tilesPerMesh*verticesPerTile, indexBufferSize, vattr);
@@ -131,9 +134,35 @@ public class TileMultiMesh
 
 			}
 	}
+	
+
+	public void updateTile(int x, int y, TileUpdate update)
+	{
+		int globalTileIdx = y * w + x;
+		
+		int meshIdx = globalTileIdx / maxTilesPerMesh;
+		// tile index
+		int tileIdx = globalTileIdx % maxTilesPerMesh;
+		// current index buffer pointer
+		int vidx = tileIdx * componentsPerTile;
+		
+		Mesh mesh = meshes.get(meshIdx);
+		
+		/////////////////////////////
+		// update tile
+		
+		update.updateTile(x, y,  vertexBufferUpdate);
+
+		mesh.updateVertices(vidx, vertexBufferUpdate);
+
+	}
 
 	public static interface TileFill
 	{
 		public void fillTile(int x, int y, int tileIdx, FloatArrayWindow vertexBuffer, ShortArrayWindow indexBuffer);
+	}
+	public static interface TileUpdate
+	{
+		public void updateTile(int x, int y, float [] vertexBufferUpdate);
 	}
 }
