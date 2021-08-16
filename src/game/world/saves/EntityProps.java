e55df64f05f218dea8569c6c71c31c0899504eb1
Fima Rotter <dveyarangi@gmail.com>
2021-08-16 18:46:03 +0300

    Create EntityProps.java

---
+++ src/game/world/saves/EntityProps.java
@@ -0,0 +1,45 @@
+package game.world.saves;
+
+import java.util.HashMap;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class EntityProps
+{
+	@Getter HashMap <String, String> props = new HashMap <> ();
+
+	public void put(String prop, int val) {
+		props.put(prop, String.valueOf(val));
+	}
+	public void put(String prop, float val) {
+		props.put(prop, String.valueOf(val));
+	}
+	public void put(String prop, String val) {
+		props.put(prop, val);
+	}
+
+	public int get(String prop)
+	{
+		if(!props.containsKey(prop))
+			throw new IllegalArgumentException("Required property " + prop + " is missing.");
+		return Integer.parseInt(props.get(prop));
+	}
+
+	public int get(String prop, int defval)
+	{
+		return props.containsKey(prop) ? Integer.parseInt(props.get(prop)) : defval;
+	}
+
+	public float get(String prop, float defval)
+	{
+		return props.containsKey(prop) ? Float.parseFloat(props.get(prop)) : defval;
+	}
+
+	public String get(String prop, String defval)
+	{
+		return props.containsKey(prop) ? props.get(prop) : defval;
+	}
+
+}
